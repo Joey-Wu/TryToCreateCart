7f9b57398c0684387ecf0ba640d746273521c871
Joey-Wu <joeywuayan@gmail.com>
2019-08-11 00:30:11 +0800

    login by session

---
+++ login.php
@@ -0,0 +1,79 @@
+<?php 
+
+session_start();
+
+// 判斷登出且跳轉至首頁
+if (isset($_GET["logout"]))
+{
+  unset($_SESSION["txtUserName"]);
+  header("Location: index.php");
+	exit();
+}
+
+// 判斷跳轉回首頁
+if (isset($_POST["btnHome"]))
+{
+	header("Location: index.php");
+	exit();
+}
+
+header("content-type:text/html; charset=utf-8");
+
+$db = new PDO("mysql:host=127.0.0.1;dbname=directory;port=3306", "root", "root");
+$db->exec("set names utf8");
+
+$sUserName = $_POST["txtUserName"];
+$sUserPassword = $_POST["txtPassword"];
+
+
+// 查詢帳號密碼
+$result = $db->query("select mempasswd from member where memName='$sUserName'");
+ 
+
+if (isset($_POST["btnOK"]))
+{
+  $row = $result->fetch();
+  // echo $row["mempasswd"];
+
+  // 判斷帳號密碼
+  if($sUserPassword == $row["mempasswd"] && $sUserName != "" && $sUserPassword != ""){ 
+    header("Location:index.php"); 
+    $_SESSION["txtUserName"] = $sUserName; 
+    } else { 
+    echo "登入失敗"; 
+  }
+	
+}
+
+?>
+
+
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Lab - Login</title>
+</head>
+<body>
+<form id="form1" name="form1" method="post" action="login.php">
+  <table width="300" border="0" align="center" cellpadding="5" cellspacing="0" bgcolor="#F2F2F2">
+    <tr>
+      <td colspan="2" align="center" bgcolor="#CCCCCC"><font color="#FFFFFF">會員系統 - 登入</font></td>
+    </tr>
+    <tr>
+      <td width="80" align="center" valign="baseline">帳號</td>
+      <td valign="baseline"><input type="text" name="txtUserName" id="txtUserName" /></td>
+    </tr>
+    <tr>
+      <td width="80" align="center" valign="baseline">密碼</td>
+      <td valign="baseline"><input type="password" name="txtPassword" id="txtPassword" /></td>
+    </tr>
+    <tr>
+      <td colspan="2" align="center" bgcolor="#CCCCCC"><input type="submit" name="btnOK" id="btnOK" value="登入" />
+      <input type="reset" name="btnReset" id="btnReset" value="重設" />
+      <input type="submit" name="btnHome" id="btnHome" value="回首頁" />
+      </td>
+    </tr>
+  </table>
+</form>
+</body>
+</html>
---
+++ product.php
@@ -0,0 +1,48 @@
+<?php
+
+session_start();
+
+if (isset($_SESSION["txtUserName"])){
+  $sUserName = $_SESSION["txtUserName"];
+}else{ 
+ header("location:login.php");
+}
+
+$db = new PDO("mysql:host=127.0.0.1;dbname=directory;port=3306", "root", "root");
+$result = $db->query("select * from product");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ProductPage</title>
+</head>
+<body>
+    <tr>Hello , <?php echo $sUserName; ?></tr>
+    <form>
+    <tr>
+    <table border="1">
+            <td>產品序號</td>
+            <td>產品名稱</td>
+            <td>單價</td>
+            <td>購買數量</td>
+        <?php while($row = $result->fetch()){ ?>
+        <tr>
+            <td><?php echo $row["pdtid"]?></td>
+            <td><?php echo $row["pdtName"]?></td>
+            <td><?php echo $row["pdtprice"]?></td>
+            <td><input type="number" min="0" max="10" value="0" name="int_count" id="intCount" /></td>
+        </tr>
+        <?php } ?>
+    </table>
+    <tr>
+    <input type="submit" name="btnOK" id="btnOK" value="結帳" />
+    <input type="reset" name="btnReset" id="btnReset" value="清空" />
+    </tr>
+    </form>
+</body>
+</html>
